429085a61588020127c7716716e00303f7821a7f
VSOPwsy <12110704@mail.sustech.edu.cn>
2023-07-08 00:23:28 +0800

    将SysTick周期设为5ms

--- Core/Src/stm32f1xx_it.c
+++ Core/Src/stm32f1xx_it.c
@@ -187,7 +187,7 @@ void SysTick_Handler(void)
   /* USER CODE END SysTick_IRQn 0 */
   HAL_IncTick();
   /* USER CODE BEGIN SysTick_IRQn 1 */
-  if (cnt < 1000)
+  if (cnt < 200)
   {
     cnt++;
   }
